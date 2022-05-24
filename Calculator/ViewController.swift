5acdfb31d49d6410f1b88e32280671f542b7fd18
Tarik <tarkztrk@icloud.com>
2022-05-24 15:28:47 +0300

    IBOutleets added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var emptyLabel: UILabel!
+    @IBOutlet var emptyLabel2: UILabel!
+    @IBOutlet var ResultLabel: UILabel!
+    @IBOutlet var ACandClearButton: UIButton!
+    @IBOutlet var plusMinusButton: UIButton!
+    @IBOutlet var percentButton: UIButton!
+    @IBOutlet var divideButton: UIButton!
+    @IBOutlet var multipleButton: UIButton!
+    @IBOutlet var minusButton: UIButton!
+    @IBOutlet var plusButton: UIButton!
+    @IBOutlet var equalButton: UIButton!
+    @IBOutlet var commaButton: UIButton!
+    @IBOutlet var zeroButton: UIButton!
+    @IBOutlet var oneButton: UIButton!
+    @IBOutlet var twoButton: UIButton!
+    @IBOutlet var threeButton: UIButton!
+    @IBOutlet var fourButton: UIButton!
+    @IBOutlet var fiveButton: UIButton!
+    @IBOutlet var sixButton: UIButton!
+    @IBOutlet var sevenButton: UIButton!
+    @IBOutlet var eightButton: UIButton!
+    @IBOutlet var nineButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
